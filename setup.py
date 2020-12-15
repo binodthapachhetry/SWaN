b7184543e4254c537a62533fe77d7e6d9f221315
Binod Thapa Chhetry <binod.thapachhetry@gmail.com>
2020-12-14 19:05:30 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     license='unlicense',
     packages= setuptools.find_packages(),
     zip_safe=False,
-    install_requires=['scikit-learn==0.22.2.post1','pandas==1.1.3','scipy==1.5.2','numpy==1.19.2']
+    install_requires=['scikit-learn==0.22.2.post1']
 )
